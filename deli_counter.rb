8a089d12147f377ba5e21d0677bc7ac54f902fe2
Eddy Tyannikov <etyannikov@gmail.com>
2019-09-18 04:30:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -25,7 +25,8 @@ def now_serving(array)
     puts "There is nobody waiting to be served!"
   else 
   puts "Currently serving #{array[0]}."
-  array = array.drop(1)
+  array.shift
+  array
 end
 end
 
